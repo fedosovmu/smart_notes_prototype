dbf5d278a24132f750d610c3bce394828d525d8a
fedosovmu <fedosovmu@gmail.com>
2023-05-20 14:00:54 +0000

    Some changes

--- main+.py
+++ main+.py
@@ -2,12 +2,8 @@ from src.csmart_notes_handler import SmartNotesHandler
 
 if __name__ == '__main__':
   # файлики с промтами
-  # prompts = ['prompt_orig.txt']
-  prompts = ['prompt_1.txt']
+  prompts = ['prompt_orig.txt']
   # файлики с заметками
-  # notes = ['note_2.txt']
   notes = ['note_1.txt', 'note_2.txt', 'note_3.txt']
-  dev = True
-  # default False
   # Выполняю запросы для всех комбинаций промтов и заметок
   SmartNotesHandler().execute(prompts, notes)
--- src/csmart_notes_handler.py
+++ src/csmart_notes_handler.py
@@ -1,65 +1,81 @@
 import os
 import openai
 import json
-
+import time
 
 class SmartNotesHandler:
-	def execute(self, prompts, notes):
-		print('====== Запускаю скрипт ======')
 
-		for prompt in prompts:
-			print(f'Выполняю промт «{prompt}»')
-			for note in notes:
-				print(f'  - Анализирую заметку «{note}»')
-				self._execute_prompt(prompt, note)
-		print('====== Конец скрипта ======')
+  def execute(self, prompts, notes):
+    print('====== Запускаю скрипт ======')
 
-	def _execute_prompt(self, prompt, note):
-		prompt_text = self._read_file_from_data(prompt)
-		note_text = self._read_file_from_data(note)
-		server_answer = self._send_request(prompt_text, note_text)
-		self._analyze_result(server_answer)
-	
-	def _read_file_from_data(self, file_name):
-		f = open(f'data/{file_name}')
-		data = f.read()
-		f.close()
-		return data
+    for prompt in prompts:
+      print(f'Выполняю промт «{prompt}»')
+      for note in notes:
+        print(f'  - Анализирую заметку «{note}»')
+        self._execute_prompt(prompt, note)
+    print('====== Конец скрипта ======')
 
-	def _send_request(self, prompt, note):
-		openai.api_key = os.getenv("API_KEY")
-		organization = os.getenv("ORGANIZATION")
+  def _execute_prompt(self, prompt, note):
+    prompt_text = self._read_file_from_data(prompt)
+    note_text = self._read_file_from_data(note)
+    start_time = time.time()
+    # print(start_time)
+    server_answer = self._send_request(prompt_text, note_text)
+    sa_time = time.time()
+    print("----------")
+    print("запрос секунд :",sa_time-start_time)
+    # print(sa_time)
+    self._analyze_result(server_answer)
+    ar_time = time.time()
+    # print(ar_time)
+    print("********")
+    print("анализ секунд :",ar_time-sa_time)
 
-		try:
-			completion = openai.ChatCompletion.create(
-			  model="gpt-3.5-turbo",
-			  messages=[
-			  	{"role": "user", "content": prompt},
-			    {"role": "user", "content": note}
-			  ]
-			)
-			server_answer = completion.choices[0].message
-			#print(f'    Ответ сервера {server_answer}')
-			return server_answer
-		except:
-			print('    Ошибка. Проблемы при отправке запроса на сервер')
-		return None
+  def _read_file_from_data(self, file_name):
+    f = open(f'data/{file_name}')
+    data = f.read()
+    f.close()
+    return data
 
-	def _analyze_result(self, server_answer):
-		if server_answer is None:
-			return
-		content = server_answer['content']
-		try:
-			events = json.loads(content)
-			print('    Распознанные события:')
-			for event in events:
-				print(f'      - "{event}"')
-		except:
-			print('    Ошибка. Не удалось распознать результат')
-			print(f'    Ответ сервера:')
-			print('=== Начало ответа ===')
-			print(content)
-			print('=== Конец ответа ===')
+  def _send_request(self, prompt, note):
+    openai.api_key = os.getenv("API_KEY")
+    organization = os.getenv("ORGANIZATION")
+    try:
+      completion = openai.ChatCompletion.create(model="gpt-3.5-turbo",
+                                                temperature=0,
+                                                messages=[{
+                                                  "role": "user",
+                                                  "content": prompt
+                                                }, {
+                                                  "role": "user",
+                                                  "content": note
+                                                }])
+      server_answer = completion.choices[0].message
+      # server_answer = completion.choices[0].message.content
+      # print(f'    Ответ сервера {server_answer}')
+      use = completion.usage
+      print(f'    Токены : {use}')
+      # if True: print(server_answer.content)
+      return server_answer
+    except:
+      print('    Ошибка. Проблемы при отправке запроса на сервер')
+    return None
+
+  def _analyze_result(self, server_answer):
+    if server_answer is None:
+      return
+    content = server_answer['content']
+    try:
+      events = json.loads(content)
+      print('    Распознанные события:')
+      for event in events:
+        print(f'      - "{event}"')
+    except:
+      print('    Ошибка. Не удалось распознать результат')
+      print(f'    Ответ сервера:')
+      print('=== Начало ответа ===')
+      print(content)
+      print('=== Конец ответа ===')
 
 
 # Серверм может ответить на один и тот же вопрос сразу в нескольких форматах. Пока что мы умеем парсить только формат 1. Я планирую доработать функцию парсинга в будущем, чтобы мы могли разпознавать более нестандартные ответы вместо того чтобы падать с ошибкой.
@@ -72,7 +88,7 @@ class SmartNotesHandler:
 # Формат 2. Json с добавлением пояснений
 # Events: ["Event 1", "Event 2"]
 # ["Event 1", "Event 2"] (Significant for narrator)
-# 
+#
 #
 # Формат 3. Произвольный список
 # Events:
